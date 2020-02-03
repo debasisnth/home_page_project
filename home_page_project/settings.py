3132899cbd206bd455748a6ca6affc72a2f3e181
debasis <debasiskool@gmail.com>
2020-02-04 02:16:19 +0530

    hh5

--- home_page_project/settings.py
+++ home_page_project/settings.py
@@ -125,10 +125,14 @@ USE_TZ = True
 
 
 
-#for server only
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
