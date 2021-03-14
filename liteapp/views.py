3a7b6336b168160ef2f91bb460bda03a972cf705
Debasis Nath <debasiskool@gmail.com>
2021-03-15 02:44:08 +0530

    15 Mar 2021 0244

--- liteapp/views.py
+++ liteapp/views.py
@@ -4,12 +4,42 @@ from django.shortcuts import render, redirect
 from django.core.mail import send_mail, BadHeaderError
 from  .forms import ContactForm
 from django.contrib import messages
+from django.utils.translation import gettext as _
 
 # Create your views here.
 
 
 
 def home(request):
+
+	desc=_('''
+	Hi, I'm Debasis Nath, I am a Python-Django,  Ionic-Angular, Core PHP, Symfony Developer and AWS consultant.
+    I have been working working as a Web developer with 3 years of experience in Django, Core Php, Symfony 2.8, Mysql, PostgreSQL, Firebase, Javascript jQuery. Have 4 years of experience in technical teaching. I am very much interested in Python Django development.
+    I have done some projects in Django the linkes are updated in mt profile link. Also, I have a good understanding of AWS, Heroku, GIT, Docker. 
+	In school my favorite subject was Chemistry, If there any project which is related to Software development and Chemistry I will dive into it :) 
+                
+	''')
+
+	alpha='https://era.thestudenthub.co.za/'
+	beta='https://meet.thestudenthub.co.za'
+	site3='https://booboocabs.com'
+	app1='https://play.google.com/store/apps/details?id=com.booboocabs.com.ionic.driver'
+	app2='https://play.google.com/store/apps/details?id=com.passenger.booboocabs.com'
+	desc2= _(f'''				
+		<p data-aos="flip-left">
+			These are some projects which have been configured on my AWS EC2 instance: <b>1. </b>
+			<a href="{alpha}"  target="_blank" > {alpha}: Symfony 2 Live Project </a>,
+			<b>2. </b><a href="{beta}"  target="_blank" > {beta}:Jeetsi Meet Server maintained by me</a>,
+			<b>3. </b><a href="{site3}"  target="_blank">{site3}:Python-Django Geo location related Live Project on Nginx(  Django ASGI Channel Chat Application integrated, development in progress...  )</a>
+			<a href="{site3}/driver.home" target="_blank" >, Driver section of the same project</a>
+			Apps I am currently working on( Ionic - Angular ): 
+			i) <b><a href="{app1}">{app1}</a> </b>
+			ii) <b><a href="{app2}">{app2}</a> </b>
+		</p>
+
+	''')
+
+
 
 	form = ContactForm()
 
@@ -46,7 +76,7 @@ def home(request):
 				pass
 
 
-	return render(request, 'liteapp/index.html', {'form': form} )
+	return render(request, 'liteapp/index.html', {'desc':desc, 'desc2':desc2, 'form': form} )
 
 
 
