84071309660015516af53412c07f0d6832d53cff
Debasis Nath <debasiskool@gmail.com>
2021-07-14 11:12:25 +0530

    make it better 14Jul2021 11:12

--- liteapp/views.py
+++ liteapp/views.py
@@ -13,11 +13,8 @@ from django.utils.translation import gettext as _
 def home(request):
 
 	desc=_('''
-	Hi, I'm Debasis Nath, I am a <u>Python-Django</u>, <u>Pandas</u>,  <u>Ionic-Angular</u>, <u>AWS( Lambda, S3, EC2, IAM etc..)</u>, <u>Serverless Framework</u>, Core PHP, Symfony Developer, Linode Expert and AWS consultant.
-    I have been working working as a Web developer with 3 years of experience in AWS(Lambda, S3, EC2, IAM etc..), Django, Core Php, Symfony 2.8, Mysql, PostgreSQL, Firebase, Javascript jQuery etc, have 4 years of experience in technical teaching. I am very much interested in Python Django development.
-    I have done some projects in Django the linkes are updated in mt profile link. Also, I have a good understanding of <u>AWS</u>, EC2, Heroku, <u>GIT</u>, Docker, <u>Rasp-Berry Pi Projects</u>. 
-	In school my favorite subject was Chemistry, If there any project which is related to Software development and Chemistry I will dive into it :) 
-                
+	Hi, I'm Debasis Nath, I am a <u>Python-Django</u>, <u>Pandas</u>,  <u>AWS</u>, <u>Serverless Framework</u>, Symfony Developer, Linode Expert and AWS consultant. I have hands on experience in AWS(Lambda, S3, EC2, IAM etc..), Django, Core Php, Symfony 2.8, Mysql, PostgreSQL, Firebase, Java-script, Angular10, jQuery etc. I have 4 years of experience in technical teaching. I am very much interested in AWS, Python, Django related development. I have done some projects in AWS, Python-Django and the links are updated in mt profile link. Also, I have a good understanding of Heroku, <u>GIT</u>, Docker, <u>Rasp-Berry Pi Projects</u>,  </u>Angular-Ionic</u> etc...  In school my favorite subject was Chemistry, If there any project which is related to Software development and Chemistry I will dive into it :)         
+	
 	''')
 
 	alpha='https://era.thestudenthub.co.za/'
@@ -27,12 +24,12 @@ def home(request):
 	app2='https://play.google.com/store/apps/details?id=com.passenger.booboocabs.com'
 	desc2= _(f'''				
 		<p data-aos="flip-left">
-			These are some projects which have been configured on my AWS EC2 instance: <b>1. </b>
+			These are some projects which have been configured on our AWS EC2 instance: <b>1. </b>
 			<a href="{alpha}"  target="_blank" > {alpha}: Symfony 2 Live Project </a>,
 			<b>2. </b><a href="{beta}"  target="_blank" > {beta}:Jeetsi Meet Server maintained by me</a>,
 			<b>3. </b><a href="{site3}"  target="_blank">{site3}:Python-Django Geo location related Live Project on Nginx(  Django ASGI Channel Chat Application integrated, development in progress...  )</a>
 			<a href="{site3}/driver.home" target="_blank" >, Driver section of the same project</a>
-			Apps I am currently working on( Ionic - Angular ): 
+			Some Ionoc-Angular Apps I worked on: 
 			i) <b><a href="{app1}">{app1}</a> </b>
 			ii) <b><a href="{app2}">{app2}</a> </b>
 		</p>
